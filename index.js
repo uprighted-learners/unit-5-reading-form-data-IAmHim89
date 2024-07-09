23822424b038bd16caded41ebb23d3f8da8745cd
IAmHim89 <computertech0418@gmail.com>
2024-07-08 21:24:06 -0400

    assignment not completed

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+//how to call your file within the folder express is in(static)
+app.use(express.static("public"));
+
+app.get("/first-word", (req, res) => {
+  res.send();
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+  console.log(`Listening on PORT ${PORT}`);
 });
